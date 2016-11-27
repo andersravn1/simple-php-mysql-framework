07545a9305b22a36bde4daa9a003126bfc3f17b7
andersravn1 <anders.ravn.adriansen@hotmail.com>
2016-11-27 21:22:10 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$mysql_host = "127.0.0.1";
-$mysql_username = "root";
-$mysql_password = "";
-$mysql_database = "messaging_system";
+$mysql_host = ""; //host of your MySQL database
+$mysql_username = ""; //username of the account which are going to be using this - DO NOT USE ROOT FOR THIS
+$mysql_password = ""; //password of the account - If no password, leave it empty
+$mysql_database = ""; //database where you wish to operate.
 $mysql_connection = mysqli_connect($mysql_host,$mysql_username,$mysql_password,$mysql_database);
 
 ?>
